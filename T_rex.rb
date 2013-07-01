88e783d0c0da6b808cfec117a36006b5664447f5
Rolf Schmutz <rschmutz@netlabs.ch>
2013-07-01 08:37:22 +0200

    T_rex as class-file only test.rb as possible application

--- T_rex.rb
+++ T_rex.rb
@@ -1,51 +1,83 @@
-#!/usr/bin/ruby -w
 # 20130628 schmutzr@post.ch
-#
+# (f) Free 2013 for post.ch
 
-PATH_SEP = "/"
+PATH_SEP = "" # seperate nodes, ie "/" for directory nodes verbatim, "" for per-character nodes (more efficient, less readable)
+INFIX_SEP = "  /  " # seperate prefix (path) from suffix (file/resource) part on output, empty if PATH_SEP not empty
 
-class Tree
-   def initialize(path)
-      @children = Hash.new
+class T_rex
+  attr_reader :node
+  def initialize(node = nil)
+    @children = Hash.new
+    @terminal = false
+    @node     = node
+    self
+  end
+
+  def member?(path)
+    path_a = self.tokenize path
+    node_name = path_a.shift
+    if node_name == @node
+      return true if path_a.empty?
+      if @children.member? path_a.first
+        child = @children[path_a.first]
+        return child.member? path_a
+      else
+        return false
+      end
+    else
+      return false
+    end
+  end
+
+  def add_child(path)
+    path_a = self.tokenize path
+    child_name = path_a.shift
+    puts "T_rex.add_child(#{path}): child_name=#{child_name}, tokenize=#{path_a.join(",")}"
+    if @children.member?(child_name)
+      child = @children[child_name]
+    else
+      child = T_rex.new(child_name)
+      @children[child_name] = child
+    end
+    if path_a.empty?
+      @terminal = true
+      puts "\tterminal"
+    else
+      child.add_child(path_a.join(PATH_SEP))
+    end
+    self
+  end
+
+  def traverse
+    result = @node
+    if not @children.empty?
+      subtree_result_a = @children.values.sort.collect{|c| c.traverse}
+      if subtree_result_a.length > 1
+        subtree_result = "(#{subtree_result_a.join("|").gsub(/[\n\r]/,"")})"
+      else
+        subtree_result = subtree_result_a.first
+      end
+      if @terminal
+        result = "(#{@node}|#{@node}#{PATH_SEP}#{subtree_result})"
+      else
+        result = "#{@node}#{PATH_SEP}#{subtree_result}"
+      end
+    end
+    return result
+  end
+
+  def <=>(b)
+    @node <=> b.node
+  end
+
+  def tokenize(path)
+    path_a = Array.new
+    if /\.[\*\?]/.match path
+      path_a[0] = path
+      puts "tokenize: take #{path} verbatim"
+    else
       path_a = path.split(PATH_SEP)
-      @node = path_a.shift
-      self.add_child(path_a.join(PATH_SEP)) if(not path_a.empty?)
-      self
-   end
-
-   def add_child(child_path)
-      child_path_a = child_path.split(PATH_SEP)
-      child_name = child_path_a.shift
-      if @children.member?(child_name)
-	 child = @children[child_name]
-      else
-	 child = Tree.new(child_name)
-	 @children[child_name] = child
-      end
-      child.add_child(child_path_a.join(PATH_SEP)) if(not child_path_a.empty?)
-      self
-   end
-
-   def traverse
-      if @children.empty?
-	 @node
-      else
-	 subtree_result_a = @children.values.collect{|c| c.traverse}
-	 if subtree_result_a.length > 1
-	    subtree_result = "(#{subtree_result_a.join("|").gsub(/[\n\r]/,"")})"
-	 else
-	    subtree_result = subtree_result_a[0]
-	 end
-	 "#{@node}#{PATH_SEP}#{subtree_result}"
-      end
-   end
+    end
+    return path_a
+  end
 end
-
-t = Tree.new(ARGF.gets)
-
-ARGF.each_line do |l|
-   break if /^$/.match l
-   t.add_child(l.chomp.sub(/^\//,""))
-end
-
-puts "#{t.traverse}#{PATH_SEP}(#{ARGF.to_a.map {|e| e.chomp} .join("|")})"
--- test.rb
+++ test.rb
@@ -1,16 +1,48 @@
-require t-rex
+#!/usr/bin/ruby -w
+# 20130630 schmutzr@post.ch
+require "T_rex"
 
-prefix = Tree.new(ARGF.gets)
+test_prefix = Array.new
+test_suffix = Array.new
 
 ARGF.each_line do |line|
-   break if /^$/.match line
-   prefix.add_child(line.chomp.sub(/^\//,""))
+  break if /^$/.match line
+  test_prefix << line.chomp
 end
 
-suffix = Tree.new(ARGF.gets)
-
 ARGF.each_line do |line|
+  test_suffix << line.chomp
+end
+
+prefix = T_rex.new(test_prefix.first)
+
+test_prefix.each do |line|
+   break if /^$/.match line
+   new_node = line.chomp # .sub(/^\//,"")
+   puts "add: #{new_node}"
+   prefix.add_child(new_node) if not prefix.member? new_node
+end
+
+suffix = T_rex.new(test_suffix.first)
+
+test_suffix.each do |line|
    suffix.add_child(line.chomp.sub(/^\//,""))
 end
 
-puts "#{prefix.traverse}#{INFIX_SEP}#{suffix.traverse}"
+puts "re: #{prefix.traverse}/?#{suffix.traverse}"
+re =  Regexp.new "#{prefix.traverse}/?#{suffix.traverse}"
+puts ""
+#exit 0
+#
+#test_prefix.each do |p|
+#  test_suffix.each do |s|
+#    test_string = "#{p}/#{s}".gsub(/\.\*/,"X")
+#    if re.match test_string
+#      puts "ok"
+#    else
+#      puts "fail: #{test_string}"
+#    end
+#  end
+#end
+#
+#
