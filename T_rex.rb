0815c0820b70fe7342aaea2287a9ecfe94562f8c
schmutzr <rschmutz@netlabs.ch>
2013-07-04 16:24:28 +0200

    still broken...

--- T_rex.rb
+++ T_rex.rb
@@ -11,9 +11,13 @@ class Array
    def tr_comparator(b) # compares self + b element wise, returns array of arrays: [ matching_elements, rest_elements_of_b ]
       # non-lispy style this time...
       split_index = ( self.zip(b).take_while { |pair| pair[0]==pair[1] } ).length
-      debug "      tr_comparator: \"#{self.join}\"=?\"#{b.join}\" -> [#{b[0..(split_index-1)]}], [#{b[split_index..-1]}]"
-      return [ [], b ] if split_index == 0
-      return [b[0..(split_index-1)], b[split_index..-1]]
+      if split_index == 0
+	 #debug "      tr_comparator: \"#{self.join}\"=?\"#{b.join}\" -> [[], #{b}]"  
+	 return [ [], b ]
+      else
+	 #debug "      tr_comparator: \"#{self.join}\"=?\"#{b.join}\" -> [#{b[0..(split_index-1)]}], [#{b[split_index..-1]}]"
+	 return [b[0..(split_index-1)], b[split_index..-1]]
+      end
    end
 end
 
@@ -45,11 +49,11 @@ class T_rex
    attr_reader :node # for <=>
    attr_writer :terminal # for add_child, ugly, needs fix
 
-   def initialize(node = Array.new, children = Hash.new)
+   def initialize(node = Array.new, children = Array.new)
       @children = children
-      @terminal = ( not node.nil? ) and children.empty?
+      @terminal = false   # ( not node.nil? ) and children.empty?
       @node     = (node.kind_of? String) ? node.split() : node
-      debug "T_rex::initialize([#{@node.join}], [#{@children.join(",") if !@children.empty?}])"
+      #debug "T_rex::initialize([#{@node.join}], [#{@children.join(",") if !@children.empty?}])"
       return self
    end
 
@@ -59,34 +63,44 @@ class T_rex
 
    def add_child(path)
       # return self if self.member? path
-
       path = path.tr_tokenize if path.kind_of? String
       (match, rest) = @node.empty? ? [ [], path ] : @node.tr_comparator(path)
-      debug "T_rex::add_child: node=[#{@node.join}], match=[#{match.join}], rest=[#{rest.join}]"
+      #debug "T_rex::add_child: node=[#{@node.join}], match=[#{match.join}], rest=[#{rest.join}]"
       case true
 	 when ( match.empty? and rest.empty? )      # terminate recursion
+	    debug "### 1"
 	    debug "T_rex::add_child: return self"	
 	    @terminal = true
 	 when ( !rest.empty? and ( @node == match or @node.empty? )) # add child, check for (partially) matching children, delegate
-	    best_matching_child_candidates = @children.values.collect do |child|
-               (m, r) = child.node.tr_comparator rest
-	       child_match_length = m.length
-               debug "   T_rex::add_child: check child [#{child.node.join}] against [#{rest.join}] match: [#{m.join}] length: #{m.length}"
-	       [ child, child_match_length ] if child_match_length > 0
-	    end
-	    if best_matching_child_candidates.empty?
-	       debug "T_rex::add_child: CREATE CHILD = [#{rest.join}]"
-	       @children[rest.join] = T_rex.new rest
+	    debug "### 2"
+	    if @children.empty?
+	       best_matching_child_candidates = []
 	    else
-               debug "T_rex::add_child: UPD #{(best_matching_child_candidates.each {|pair| "#{pair[0].node.join}=#{pair[1]}"}).join(",")}"
+	       best_matching_child_candidates = @children.collect do |child|
+		  #debug "   T_rex::add_child: check child \"#{child.to_s}\""
+		  (m, r) = child.node.tr_comparator rest
+		  child_match_length = m.length
+		  #debug "   T_rex::add_child: check child [#{child.node.join}] against [#{rest.join}] match: [#{m.join}] length: #{m.length}"
+		  [ child, child_match_length ] if child_match_length > 0
+	       end
+	    end
+	    best_matching_child_candidates.compact!
+	    if best_matching_child_candidates.empty?
+	       debug "T_rex::add_child: CREATE CHILD : parent=\"#{@node.join}\"   [#{rest.join}]"
+	       @children << T_rex.new(rest)
+	    else
+	       #debug "T_rex::add_child: update candidates length=#{best_matching_child_candidates.length} [#{best_matching_child_candidates.to_s}]"
 	       best_matching_child = (best_matching_child_candidates.sort {|a,b| a[1]<=>b[1]})[-1][0]
-	       debug "T_rex::add_child: UPDATE CHILD [#{best_matching_child.node.join}] <- [#{rest.join}]"
+	       debug "T_rex::add_child: UPDATE CHILD : parent=\"#{@node.join}\"   [#{best_matching_child.node.join}] <- [#{rest.join}]"
 	       best_matching_child.add_child rest
 	    end
 	 when @node.length > match.length           # split
-	    # debug "T_rex::add_child: split"
+	    debug "### 3"
+	    debug "T_rex::add_child: SPLIT        : \"#{@node.join}\" -> \"#{@node[0..(match.length-1)].join}\", children \"#{@node[match.length..-1].join}\" and \"#{rest.join}\""
 	    # "clone" this node with non-matching path
-	    @children = [ T_rex.new(@node[match.length..-1], @children), T_rex.new(@rest) ]
+	    split_child_path = @node[match.length..-1]
+	    new_children = [ T_rex.new(split_child_path, @children), T_rex.new(rest) ] # slighly confusing... this node has only two children, the first one inherits all our previous children
+	    @node = @node[0..(match.length-1)]
 	 else debug "T_rex::add_child: return self (fall-through)"
       end
 
@@ -96,7 +110,7 @@ class T_rex
    def make_re
       result = ""
       if not @children.empty?
-	 subtree = @children.values.sort.collect { |child| child.make_re } 
+	 subtree = @children.collect { |child| child.make_re } 
 	 if subtree.length==1 and not @terminal
 	    result = "#{subtree.first}"
 	 else
@@ -108,7 +122,7 @@ class T_rex
 
    def make_dot(path=nil)
       path="#{path}#{@node.join if not @node.nil?}"
-      subtree_dot = @children.values.sort.collect { |child| [ "#{self.object_id} -> #{child.object_id}", "#{child.make_dot(path)}" ] } if not @children.empty? 
+      subtree_dot = @children.sort.collect { |child| [ "#{self.object_id} -> #{child.object_id}", "#{child.make_dot(path)}" ] } if not @children.empty? 
       node_dot    = "#{self.object_id} [label=\"#{(@terminal) ? path : ((@node.nil?) ? "" : @node.join)}\",tooltip=\"#{path}\"#{",style=\"filled\"" if @terminal}]"
       return [ node_dot, subtree_dot ].compact.flatten.join(";\n")
    end
